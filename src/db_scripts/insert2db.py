13dd12fdd2060ea7a10c8838890c6b745965fdd6
Yiannis Sotiropoulos <yiannis.sotiropoulos@gfk.com>
2021-06-20 22:57:33 +0200

    DBSCRIPTS: final inser2db

--- src/db_scripts/insert2db.py
+++ src/db_scripts/insert2db.py
@@ -1,7 +1,10 @@
 from os import path
+from kombu.exceptions import OperationalError
 import pandas as pd
 import sys
 from celery.utils.log import get_task_logger
+from sqlalchemy import create_engine
+from sqlalchemy.exc import DatabaseError
 
 # Create logger - enable to display messages on task logger
 celery_log = get_task_logger(__name__)
@@ -18,6 +21,18 @@ from utils.config_loader import load
 # use default value for config file
 config = load()
 
+# TODO: fix invalid literal for int() with base 10 for integer env vars
+DB_PORT = config.get('credentials', 'DB_PORT')
+DB_USER = config.get('credentials', 'DB_USER')
+DB_PASS = config.get('credentials', 'DB_PASSWORD')
+DB_NAME = config.get('credentials', 'DB_NAME')
+DB_HOST = config.get('credentials', 'DB_HOST')
+# db engine
+# api_ml_postgres
+engine = create_engine(
+    f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    )
+
 def dbwrite():
     """
     This function just writes the csv file in the data.
@@ -30,5 +45,18 @@ def dbwrite():
     celery_log.info(metadata_path)
     images_path = path.join(data_folder, images)
     
-    df = pd.read_csv(metadata_path, sep=',', header = 'infer',error_bad_lines = False)
-    celery_log.info(df)
+    df = pd.read_csv(metadata_path,
+                     sep=',',
+                     header = 'infer',
+                     error_bad_lines = False)
+    df['url'] = df['id'].map(
+        lambda x: path.join(images_path, str(x))
+        )
+    df.set_index('id', inplace=True)
+    try:
+        df.to_sql('styles_data', engine, if_exists='replace',index=True)
+    except DatabaseError as e:
+        celery_log.error(f'No connection to DB: {e}')
+        return 'Not ok'
+
+    return 'ok'
